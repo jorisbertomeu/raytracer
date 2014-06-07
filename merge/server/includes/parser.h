5ec58fcda6d3432dd0f193109fd916c1f916a4f4
Joris Bertomeu <joris.bertomeu@epitech.eu>
2014-06-07 16:47:04 +0200

    r

--- merge/server/includes/parser.h
+++ merge/server/includes/parser.h
@@ -5,7 +5,7 @@
 ** Login   <merran_g@epitech.net>
 ** 
 ** Started on  Mon May 19 23:05:46 2014 Geoffrey Merran
-** Last update Fri Jun  6 03:19:18 2014 Geoffrey Merran
+** Last update Sat Jun  7 16:46:57 2014 Joris Bertomeu
 */
 
 #ifndef PARSER_
@@ -39,7 +39,6 @@ void		add_to_end(t_list *, char *);
 
 void   	add_item(t_node **list, t_item new);
 void	init_item(t_item *new, t_vector pos, t_vector angle, t_type type);
-void	init_item2(t_item *new, float rayon, t_rgb color, float brillance, float reflexion);
 void	init_rgb(t_rgb *color, int r, int g, int b);
 t_node 	*get_items();
 t_spot 	*get_spots();
